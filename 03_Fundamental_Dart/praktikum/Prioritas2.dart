a94b62693b81232214ebc3a287645907e64d7457
MochAgatha <mochagatha271@gmail.com>
2023-08-16 18:35:10 +0700

    persegi

---
+++ 03_Fundamental_Dart/praktikum/Prioritas2.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+void main() {
+String nama = ("Agatha");
+String nama2 = ("Latifah");
+String nama3 = ("Kyy");
+print("Hallo, nama saya $nama mempunyai teman bernama $nama2 dan $nama3");
+  print("=========================================================");
+  int r = 5 ;
+  int t = 10;
+  var rumus = pi * (r * r) * t;
+
+  print("Volume Tabung = $rumus");
+}
