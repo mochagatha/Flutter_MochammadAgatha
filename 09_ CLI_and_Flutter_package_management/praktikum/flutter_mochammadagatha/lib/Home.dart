e6676d648510dad019210b54dfbbcb10a620bde4
MochAgatha <mochagatha271@gmail.com>
2023-09-02 12:29:05 +0700

    tugas 9

---
+++ 09_ CLI_and_Flutter_package_management/praktikum/flutter_mochammadagatha/lib/Home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Halo Alterra",
+          style: GoogleFonts.akronim(
+            color: Colors.black,
+            fontSize: 32,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 83, 97, 108),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Text(
+                "Nama saya adalah Mochammad Agatha Rahmawan Putra",
+                style: GoogleFonts.roboto(
+                  color: const Color(0xFF4D4D4D),
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
