482517dc536dea2ec63414b78b7f7b6d2d1577f9
MochAgatha <mochagatha271@gmail.com>
2023-09-21 00:04:17 +0700

    revisi tugas 12

---
+++ 12_Assets_Dialog_Bottomsheet_Navigation/praktikum/flutter_mochammadagatha/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_mochammadagatha/view/create_new_contacts.dart';
+import 'package:flutter_mochammadagatha/view/gridview_screen.dart';
+import 'package:flutter_mochammadagatha/view/show_image.dart';
+// import 'package:flutter_mochammadagatha/view/create_new_contacts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      // home: const createNewContacts(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (_) => const gridviewScreen(),
+        '/showImage': (_) => const showImage()
+      },
+    );
+  }
+}
