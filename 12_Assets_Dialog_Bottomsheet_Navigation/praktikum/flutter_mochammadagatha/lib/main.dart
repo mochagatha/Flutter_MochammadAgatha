4251496107bd7c45aec272308e53123c9262d8ec
MochAgatha <mochagatha271@gmail.com>
2023-09-23 13:28:54 +0700

    revisi

--- 12_Assets_Dialog_Bottomsheet_Navigation/praktikum/flutter_mochammadagatha/lib/main.dart
+++ 12_Assets_Dialog_Bottomsheet_Navigation/praktikum/flutter_mochammadagatha/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_mochammadagatha/view/create_new_contacts.dart';
 import 'package:flutter_mochammadagatha/view/gridview_screen.dart';
 import 'package:flutter_mochammadagatha/view/show_image.dart';
-// import 'package:flutter_mochammadagatha/view/create_new_contacts.dart';
+import 'package:flutter_mochammadagatha/view/create_new_contacts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,11 +16,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.light,
       ),
-      // home: const createNewContacts(),
       debugShowCheckedModeBanner: false,
       routes: {
         '/': (_) => const gridviewScreen(),
-        '/showImage': (_) => const showImage()
+        '/showImage': (_) => const showImage(),
+        '/contacts': (_) => const createNewContacts(),
       },
     );
   }
